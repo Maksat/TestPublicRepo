a7296feab2c061964b34b0d9d9221efe485f0187
Maksat Amangeldiyev <amangeldiyev.m@gmail.com>
2012-05-09 11:48:10 +0300

    ergrefer

---
+++ ertrete/ertrete/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ertrete
+//
+//  Created by Maksat Amangeldiyev on 5/9/12.
+//  Copyright (c) 2012 Dunya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
