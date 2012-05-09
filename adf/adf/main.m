402299ba028a5cf1087408fb9cf8b4ff77ca74a4
Maksat Amangeldiyev <amangeldiyev.m@gmail.com>
2012-05-09 14:00:51 +0300

    f

---
+++ adf/adf/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  adf
+//
+//  Created by Maksat Amangeldiyev on 5/9/12.
+//  Copyright (c) 2012 Dunya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
